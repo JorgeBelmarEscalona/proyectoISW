fc13296ac3d1c84b9691eaaec4c272d7ec603295
jorge <jorgeguillermobelmar@gmail.com>
2023-10-18 15:08:04 -0300

    Primer commit en mi branch

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -17,7 +17,7 @@ const express = require('express');
 const app = express();
 
 // Agrega el middleware para el manejo de datos en formato JSON
-//app.use(express.json());
+app.use(express.json());
 
 //app.use(indexRouters);
 
