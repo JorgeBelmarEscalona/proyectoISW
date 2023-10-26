e7d8e94e8968be6b18befc80fa9efec4e7864c26
jorge <jorgeguillermobelmar@gmail.com>
2023-10-26 00:08:36 -0300

    Creacion denuevo de index.js en routes

--- BACKEND/src/controllers/beneficio.controller.js
+++ BACKEND/src/controllers/beneficio.controller.js
@@ -42,7 +42,7 @@ exports.deleteBeneficio = async (req, res) => {
         if (!beneficio) {
             return res.status(404).json({ message: 'Beneficio not found' });
         }
-        await beneficio.remove();
+        await beneficio.findByIdandDelete();
         res.status(200).json({ message: 'Beneficio deleted successfully' });
     } catch (err) {
         res.status(500).json({ message: err.message });
